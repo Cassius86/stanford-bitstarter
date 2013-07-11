63a713fa15b2ac4d4188bdb80c8dacfe801cd02c
Ubuntu <ubuntu@ip-172-31-36-104.eu-west-1.compute.internal>
2013-07-11 11:31:56 +0000

    Grader modifications

--- grader.js
+++ grader.js
@@ -26,6 +26,10 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "//http://powerful-caverns-4862.herokuapp.com";
+
+var rest = require('restler');
+
 
 var assertFileExists = function(infile) {
   var instr = infile.toString();
@@ -52,7 +56,7 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     var present = $(checks[ii]).length > 0;
     out[checks[ii]] = present;
   }
-  return out;
+  console.log(JSON.stringify(out, null, 4));
 };
 
 var clone = function(fn) {
@@ -61,14 +65,31 @@ var clone = function(fn) {
   return fn.bind({});
 };
 
+var checkUrl = function(url, checksfile) {
+  rest.get(url).on('complete', function(data) {
+    $ = cheerio.load(data.toString());
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+      var present = $(checks[ii]).length > 0;
+      out[checks[ii]] = present;
+    }
+    console.log(JSON.stringify(out, null, 4));
+  });
+}
+
 if(require.main == module) {
   program
     .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+    .option('-u, --url <url>', 'URL to index.html', URL_DEFAULT)
     .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
     .parse(process.argv);
-  var checkJson = checkHtmlFile(program.file, program.checks);
-  var outJson = JSON.stringify(checkJson, null, 4);
-  console.log(outJson);
+  if (program.url){
+    checkUrl(program.url, program.checks);
+  } else {
+    checkHtmlFile(program.file, program.checks);
+  }
+
 } else {
   exports.checkHtmlFile = checkHtmlFile;
 }
